08abb25cdd585f5303adbb9cb6549aa92821d1d2
Benat Castro <becastro@c4r5s3.42urduliz.com>
2022-07-17 22:11:03 +0200

    walls parsing wip

--- srcs/so_long/map.c
+++ srcs/so_long/map.c
@@ -6,42 +6,64 @@
 /*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 15:56:49 by becastro          #+#    #+#             */
-/*   Updated: 2022/07/17 20:16:31 by becastro         ###   ########.fr       */
+/*   Updated: 2022/07/17 22:08:33 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static int	ft_check_walls(t_map map_data, char *map_line)
-{
-	int	i;
+// static int	ft_check_walls(t_map map_data, char *map_line)
+// {
+// 	int	i;
 
-	(void)map_data;
+// 	(void)map_data;
+// 	i = -1;
+// 	while (map_line[++i])
+// 	{
+// 		if (map_line[i] != '1')
+// 		{
+// 			perror("Walls error");
+// 			exit(EXIT_FAILURE);
+// 		}
+// 	}
+// 	return (1);
+// }
+
+static void	ft_parse_map(t_map map_data, char *map_line)
+{
+	static int	line;
+	int			i;
+	int			len;
+
+	len = ft_strlen(map_line) - 2;
+	printf("test: %d|| %s \n", map_data.map_width, map_line);
 	i = -1;
-	while (map_line[++i])
+	if (map_data.map_width != len)
+		printf("error3");
+	if (map_line[0] != '1' || map_line[len] != '1')
+		printf("Error2\n");
+	while (map_line[++i] && i < len)
 	{
-		if (map_line[i] != '1')
+		if (map_line[i] != '1' && line == 0)
 		{
-			perror("Walls error");
+			printf("CHAR (%d) i (%d)", map_line[i], i);
+			printf("Error\n");
 			exit(EXIT_FAILURE);
 		}
 	}
-	return (1);
-}
-
-static void	ft_parse_map(t_map map_data, int line_n, char *map_line)
-{
-	if (line_n == 0 && ft_check_walls(map_data, map_line))
-		map_data.map_width = ft_strlen(map_line);
+	if (line == 0)
+	{
+		printf("HELLO");
+		map_data.map_width = len;
+	}
+	line++;
 }
 
 static void	ft_get_map_line(t_map map_data)
 {
 	char	*map_line;
 	int		check;
-	int		line;
 
-	line = -1;
 	check = 0;
 	map_line = NULL;
 	while (map_line || check == 0)
@@ -49,10 +71,7 @@ static void	ft_get_map_line(t_map map_data)
 		check = 1;
 		map_line = get_next_line(map_data.fd);
 		if (map_line)
-		{
-			line++;
-			ft_parse_map(map_data, line, map_line);
-		}
+			ft_parse_map(map_data, map_line);
 		free(map_line);
 	}
 }
