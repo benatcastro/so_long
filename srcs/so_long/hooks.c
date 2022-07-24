f788db33cd29663dac988e56ac1246c9b39093dc
becastro <benatcastro@gmail.com>
2022-07-25 00:58:40 +0200

    hooks.c updated with t_character as second param added test move fncs

--- srcs/so_long/hooks.c
+++ srcs/so_long/hooks.c
@@ -6,16 +6,27 @@
 /*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 15:24:40 by becastro          #+#    #+#             */
-/*   Updated: 2022/07/25 00:15:22 by bena             ###   ########.fr       */
+/*   Updated: 2022/07/25 00:55:31 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	ft_key_hooks(int key)
+int	ft_key_hooks(int key, void *player_data)
 {
+	t_character	*player;
+
+	player = player_data;
 	//printf("KEY (%d)\n", key);
 	if (key == ESC || key == CLOSE_BUTTON)
 		exit (0);
-	return (1);
+	if (key == W)
+		ft_move_character(player, 0);
+	if (key == S)
+		ft_move_character(player, 1);
+	if (key == A)
+		ft_move_character(player, 2);
+	if (key == D)
+		ft_move_character(player, 3);
+	return (key);
 }
