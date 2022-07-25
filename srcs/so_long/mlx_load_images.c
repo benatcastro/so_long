32145106d4d6cf1b0c01cdea43bd35118398ee29
Benat Castro <becastro@c4r3s4.42urduliz.com>
2022-07-25 22:58:25 +0200

    placeholder textures for wall edges

--- srcs/so_long/mlx_load_images.c
+++ srcs/so_long/mlx_load_images.c
@@ -3,31 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   mlx_load_images.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: becastro <becastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 15:24:40 by becastro          #+#    #+#             */
-/*   Updated: 2022/07/25 00:08:33 by bena             ###   ########.fr       */
+/*   Updated: 2022/07/25 21:52:18 by becastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "textures.h"
 #include "so_long.h"
 
+void	ft_load_w_edges(t_program_data *data)
+{
+	int			res;
+
+	res = IMG_RES;
+	data->w_edges.tex[0] = mlx_xpm_file_to_image(data->mlx.ptr,
+			"textures/red_test.xpm", &res, &res);
+	data->w_edges.tex[1] = mlx_xpm_file_to_image(data->mlx.ptr,
+			"textures/test1.xpm", &res, &res);
+}
+
 void	ft_load_character(t_program_data *data)
 {
 	int			res;
 
 	res = IMG_RES;
-
 	data->player.tex[0] = mlx_xpm_file_to_image(data->mlx.ptr,
-			"textures/red_test.xpm", &res, &res);
-	data->player.tex[1] = mlx_xpm_file_to_image(data->mlx.ptr,
 			"textures/green_test.xpm", &res, &res);
-	data->player.tex[2] = mlx_xpm_file_to_image(data->mlx.ptr,
+	data->player.tex[1] = mlx_xpm_file_to_image(data->mlx.ptr,
 			"textures/blue_test.xpm", &res, &res);
 }
 
 void	ft_load_textures(t_program_data *data)
 {
+	ft_load_w_edges(data);
 	ft_load_character(data);
 }
